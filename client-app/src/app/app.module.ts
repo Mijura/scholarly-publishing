a1a11a9759bfb369e71a54f0e7f8508dbf39ca02
Mijura <miodragvilotijevic@gmail.com>
2020-01-09 23:50:09 +0100

    installed angular material

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AddPaperComponent } from './add-paper/add-paper.component';
 import { RoutingModule } from './app.routing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { RoutingModule } from './app.routing';
   ],
   imports: [
     RoutingModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
