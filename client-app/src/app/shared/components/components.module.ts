b06b88351f97f5a58d5ac57284e9209c16f09d07
Mladen <diskozub@hotmail.com>
2020-02-03 11:02:49 +0100

    fix material module path

--- client-app/src/app/shared/components/components.module.ts
+++ client-app/src/app/shared/components/components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaperEditorComponent } from './paper-editor/paper-editor.component';
-import { MaterialModule } from 'src/app/material.module';
+import { MaterialModule } from '../../material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AutosizeModule } from 'ngx-autosize';
 import { KeywordDialogComponent } from './keyword-dialog/keyword-dialog.component';
